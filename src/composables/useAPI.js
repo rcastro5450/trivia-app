05f8fd2e8d4f336122e69ae7efc18bce2f8dddeb
Roel Castro <rcastro_5450@stu.southtexascollege.edu>
2025-03-16 13:18:26 -0500

    GetQuestion is working

--- src/composables/useAPI.js
+++ src/composables/useAPI.js
@@ -8,13 +8,23 @@ const instance = axios.create({
 const categories = ref([])
 
 export default function useAPI() {
-    const getCategories = async () => { 
+    const getCategories = async () => {
         if (categories.value.length === 0) {
             const response = await instance.get('api_category.php')
             categories.value = response.data.trivia_categories
         }
     }
 
+    const getQuestion = async (categoryId) => {
+        const response = await instance.get('api.php', {
+            params: {
+                amount: 1,
+                category: categoryId,
+            }
+        })
 
-    return { instance, categories, getCategories}
+        return response.data.results[0]
+    }
+
+    return {instance, categories, getCategories, getQuestion}
 }
